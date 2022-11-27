b853b0f17b66c32573d5d7f4d4897fa8581dd2e7
Zachary Vance <za3k@za3k.com>
2022-11-27 16:51:25 -0500

    Add link

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_info({
     "project_name": "Hack-A-Minigame",
     "source_url": "https://github.com/za3k/day26_mini",
     "subdir": "/hackaday/mini",
-    "description": "A very tiny minigame, where your only controls are save/restore",
+    "description": "A very tiny minigame, where your only controls are save/restore. To play yourself, visit <a href=\"https://tilde.za3k.com/hackaday/snake\">Hack-A-Snake</a>",
     "instructions": "This is the classic game Snake, played by an AI pressing random buttons. Your only power is to save and load the game, using three save slots.",
     "login": True,
     "fullscreen": False,
